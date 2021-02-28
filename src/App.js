3a2043b5401290c6552bd388977b876dab63c1e6
Sajal Mishra <sajalmishra44@gmail.com>
2021-02-28 16:45:32 +0530

    Styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Reeact ,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Filter from "./components/filter";
 import Columns from "./data/columns"
@@ -6,15 +6,25 @@ import Table from "./components/table"
 import data from "./data/tabledata"
 function App() {
   const [conditions, setConditions] = useState([]);
-  useEffect(()=>{
+  useEffect(() => {
     console.log(conditions)
   }, [conditions])
 
   return (
-    <div className="App">
-      <h1>Filter-Table</h1>
-      <Filter conditions = {conditions} setConditions = {setConditions} />
-      <Table column = {Columns} data = {data}/>
+    <div style={{
+      display: "flex",
+      flex: 1,
+      backgroundColor: "pink",
+      justifyContent: "center",
+      alignItems: "center",
+      width: '100%',
+      flexDirection: "column",
+      minHeight: "100vh"
+    }}>
+      <h1>Filter Table</h1>
+      <Filter conditions={conditions} setConditions={setConditions} />
+      <Table column={Columns} data={data} />
+      <text style={{ marginBottom: 20, color: "grey", display: "inline" }}>&copy; Sajal Kumar Mishra</text>
     </div>
   );
 }
--- src/components/filterRow.js
+++ src/components/filterRow.js
@@ -1,51 +1,55 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Dropdown from "./dropdown";
 import Columns from "../data/columns"
-import {Operators,Cond}     from "../data/operators"
-const FilterRow = ({data, addFilter, deleteFilter}) =>{
+import { Operators, Cond } from "../data/operators"
+const FilterRow = ({ data, addFilter, deleteFilter }) => {
     const [column, setColumn] = useState({});
     const [operator, setOperator] = useState({});
     const [value, setValue] = useState();
     const [op, setOP] = useState([])
-    const [cond ,setCond] = useState();
-    const [ isDisable, setIsDisable] = useState(false);
-    useEffect(()=>{
-        if(column.type){
-          setOP(  Operators.filter( e => e.type === column.type))
+    const [cond, setCond] = useState();
+    const [isDisable, setIsDisable] = useState(false);
+    useEffect(() => {
+        if (column.type) {
+            setOP(Operators.filter(e => e.type === column.type))
         }
-    },[column])
-    const handleValue = (e) =>    setValue(e.target.value);
-    const handleAdd   = (e) => {
-        if(cond &&column && operator && value){
+    }, [column])
+    const handleValue = (e) => setValue(e.target.value);
+    const handleAdd = (e) => {
+        if (cond && column && operator && value) {
             let obj = {
-                id :data.id,
-                data : {
-                  id : column.key,
-                  operator :operator.key,
-                  value : value
+                id: data.id,
+                data: {
+                    id: column.key,
+                    operator: operator.key,
+                    value: value
                 },
-                cond : cond.key
-              }
-              setIsDisable(true)
-              addFilter(obj)
-        }else{
+                cond: cond.key
+            }
+            setIsDisable(true)
+            addFilter(obj)
+        } else {
             alert("invalid filter")
         }
 
     }
     const handleDelete = (e) => {
-        deleteFilter({id : data.id})
+        deleteFilter({ id: data.id })
     }
-    
-    
-    return(
-        <div id = {data.id}>
-             <Dropdown setValue={setCond} opctions={Cond} />
+
+
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: "space-around",
+            flexWrap: "wrap"
+        }} id={data.id}>
+            <Dropdown setValue={setCond} opctions={Cond} />
             <Dropdown setValue={setColumn} opctions={Columns} />
             <Dropdown setValue={setOperator} opctions={op} />
-            <input  style={{ width: 100 }} placeholder="value" type = "text" value={value} onChange= {handleValue}/>
-            <button disabled={isDisable} type= "button" onClick = {handleAdd}> ADD </button>
-            <button type = "button" onClick ={handleDelete}> Delete</button>
+            <input style={{ width: 100, margin: 20 }} placeholder="value" type="text" value={value} onChange={handleValue} />
+            <button style={{ width: 100, margin: 20 }} disabled={isDisable} type="button" onClick={handleAdd}> Add </button>
+            <button style={{ width: 100, margin: 20 }} type="button" onClick={handleDelete}> Delete</button>
         </div>
     )
 
