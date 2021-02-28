3b6d787db2946af5b9fe10621926f0a118331d09
Sajal Mishra <sajal.mishra@radiatechs.com>
2021-02-28 16:50:10 +0530

    tmp

--- src/App.js
+++ src/App.js
@@ -6,15 +6,28 @@ import Table from "./components/table"
 import data from "./data/tabledata"
 function App() {
   const [conditions, setConditions] = useState([]);
+  const [tabledata,setTabledata]  = useState(data);
+
   useEffect(()=>{
     console.log(conditions)
   }, [conditions])
+  const isvalid = obj => obj.id && obj.data.id && obj.data.operator && obj.data.value && obj.cond ;
+  
+const applyfilter = () =>{
+  conditions.forEach((index, obj)=>{
+    if(isvalid(obj)){
+      Table.forEach((ind,oj)=>{
 
+      })
+
+    }
+  })
+}
   return (
     <div className="App">
       <h1>Filter-Table</h1>
       <Filter conditions = {conditions} setConditions = {setConditions} />
-      <Table column = {Columns} data = {data}/>
+      <Table column = {Columns} data = {tabledata}/>
     </div>
   );
 }
---
+++ src/util/operators.js
@@ -0,0 +1,3 @@
+module.exports.gte = (a,b)=>{
+
+}
